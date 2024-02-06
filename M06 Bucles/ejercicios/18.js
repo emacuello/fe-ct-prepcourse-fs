d13d8c5ac892206c414802a9456dae3953e224d4
Emanuel Cuello <ema.cuello1010@gmail.com>
2024-02-06 11:51:51 -0300

    fix

--- M06 Bucles/ejercicios/18.js
+++ M06 Bucles/ejercicios/18.js
@@ -3,22 +3,16 @@ function productoEntreNúmeros(a, b) {
   // los números entre a y b (inclusive).
   // Tu código:
   
-
-  // Invierte los valores si a es mayor que b
   if (a > b) {
     [a, b] = [b, a];
   }
 
-  // Inicializa el producto como 1
   let producto = 1;
-
-  // Calcula el producto de todos los números entre a y b (inclusive) con un bucle while
-  let current = a;
-  while (current <= b) {
-    producto *= current;
-    current++;
+  let arg = a;
+  while (arg <= b) {
+    producto *= arg;
+    arg++;
   }
-
   //no entiendo porque el test daba -0, asi que tuve que realizar este arreglo para que pase el test, o.o
   if (producto === -0) return 0;
   else return producto;
